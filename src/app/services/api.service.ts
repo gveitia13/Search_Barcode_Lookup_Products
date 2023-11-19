aa3a92e5cceda948a566dc62f8760c2cd41a6c4c
Gilbert Veitia Urbay <61420914+gveitia13@users.noreply.github.com>
2023-11-19 17:19:49 -0500

    create proxy config to avoid the cors policy

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,5 @@
 import {inject, Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {environment} from "@src/environments/environment";
 
 @Injectable({
@@ -10,8 +10,12 @@ export class ApiService {
   private key = 'key=' + environment.key
   private url = environment.url
 
+
   getByBarcode(barcode: string) {
-    return this.http.get(this.url + '?barcode=' + barcode + '&formatted=y&' + this.key)
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'})
+    };
+    return this.http.get(this.url + '?barcode=' + barcode + '&formatted=y&' + this.key, httpOptions)
   }
 
   getMax10ByBarcode(barcodeArray: string[]) {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   key: 'kni8877poau6et64972li7n9focmtv',
-  url: 'https://api.barcodelookup.com/v3/products'
+  url: '/v3/products'
 };
